669f5c9689176bf2b2f99f1b961e530db8280aa6
Gabe Nunez <gabe@gabenunez.com>
2023-09-18 22:25:10 -0700

    Update meta to prevent mobile zoom

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,6 +17,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, maximum-scale=1"
+      />
       <body className={inter.className}>{children}</body>
     </html>
   );
