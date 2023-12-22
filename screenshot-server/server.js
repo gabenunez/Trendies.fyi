a6117e28d08a1e7c5eb1de4774b702036982193a
Gabe Nunez <gabe@gabenunez.com>
2023-12-22 10:52:37 -0600

    Move strictly to pupeteer

--- screenshot-server/server.js
+++ screenshot-server/server.js
@@ -1,6 +1,6 @@
 import "dotenv/config";
 import express from "express";
-import captureWebsite from "capture-website";
+import puppeteer from "puppeteer";
 
 const app = express();
 app.use(express.json());
@@ -9,14 +9,25 @@ app.post("/", async (req, res) => {
   const { url } = req.body;
 
   try {
-    const base64Image = await captureWebsite.base64(url, {
-      waitForElement: "#graph-area",
-      element: "#graph-area",
-      timeout: 5,
-      styles: [".rounded-lg {border-radius: 0;}"],
-    });
+    // Launch the browser and open a new blank page
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
 
-    return res.json(base64Image);
+    // Navigate the page to a URL
+    await page.goto(url);
+
+    // Set screen size
+    await page.setViewport({ width: 1830, height: 830 });
+
+    // Select graph area
+    const element = await page.waitForSelector("#graph-area");
+
+    const graphScreenshot = await element.screenshot();
+    const base64Image = Buffer.from(graphScreenshot).toString("base64");
+
+    await browser.close();
+
+    return res.json({ image: base64Image });
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: "Unable to fetch image." });
