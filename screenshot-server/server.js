1324a96d007cb548f01baceb13ab358bc149bd85
Gabe Nunez <gabe@gabenunez.com>
2023-12-23 16:02:59 -0600

    Logging to help diagnose the problem

--- screenshot-server/server.js
+++ screenshot-server/server.js
@@ -16,27 +16,38 @@ app.post("/", async (req, res) => {
       .json({ error: "Sorry, this is an invitation only kinda party." });
   }
 
+  console.log("1. Trying the following URL", url);
+
   try {
     // Launch the browser and open a new blank page
     const browser = await puppeteer.launch({ headless: "new", timeout: 0 });
     const page = await browser.newPage();
+    console.log("2. Browser page opened");
 
     // Set screen size
     await page.setViewport({ width: 1851, height: 698 });
+    console.log("3. Viewport set");
 
     // Navigate the page to a URL
     await page.goto(url, { waitUntil: "domcontentloaded" });
+    console.log("4. Navigated to URL.");
 
     // Apply styling
     await page.addStyleTag({ content: ".rounded-lg {border-radius: 0;}" });
+    console.log("5. Added styling");
 
     // Select graph area
     const element = await page.waitForSelector("#graph-area");
+    console.log("6. Selected graph-area element");
 
     const graphScreenshot = await element.screenshot();
+    console.log("7. screenshot taken");
+
     const base64Image = Buffer.from(graphScreenshot).toString("base64");
+    console.log("8. Screenshoy converted to base64");
 
     await browser.close();
+    console.log("9. browser closed");
 
     return res.json({ image: base64Image });
   } catch (error) {
