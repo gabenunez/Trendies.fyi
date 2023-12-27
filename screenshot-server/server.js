132de756511829b193baa1a1487789b9510c549c
Gabe Nunez <gabe@gabenunez.com>
2023-12-26 23:24:03 -0600

    Concurrency limits using puppeteer cluster, this should help a ton!

--- screenshot-server/server.js
+++ screenshot-server/server.js
@@ -2,28 +2,20 @@ import "dotenv/config";
 import express from "express";
 import puppeteer from "puppeteer";
 import { put } from "@vercel/blob";
+import { Cluster } from "puppeteer-cluster";
 
 const app = express();
 app.use(express.json());
 
-app.post("/", async (req, res) => {
-  const { url } = req.body;
-
-  if (
-    req.header("internal-secret") !== process.env.SVT_INTERNAL_REQUEST_SECRET
-  ) {
-    return res
-      .status(401)
-      .json({ error: "Sorry, this is an invitation only kinda party." });
-  }
-
-  try {
-    res.json({ status: "Processing request!" });
-
-    // Launch the browser and open a new blank page
-    const browser = await puppeteer.launch({ headless: "new" });
-    const page = await browser.newPage();
-
+(async () => {
+  const cluster = await Cluster.launch({
+    concurrency: Cluster.CONCURRENCY_CONTEXT,
+    maxConcurrency: 1,
+    puppeteerOptions: {
+      headless: "new",
+    },
+  });
+  await cluster.task(async ({ page, data: url }) => {
     // Set screen size
     await page.setViewport({ width: 1851, height: 698 });
 
@@ -39,26 +31,43 @@ app.post("/", async (req, res) => {
     // Take a screenshot
     const graphScreenshot = await element.screenshot();
 
-    // Close out the browser
-    await browser.close();
+    return graphScreenshot;
+  });
 
-    // Add it to the blob store!
-    const data = await put(btoa(url) + ".png", graphScreenshot, {
-      access: "public",
-      addRandomSuffix: false,
-    });
-  } catch (error) {
-    console.error(error);
-    return res.status(500).json({ error: "Unable to fetch image." });
-  }
-});
+  app.post("/", async (req, res) => {
+    const { url } = req.body;
 
-app.head("/", async (req, res) => {
-  return res.json({ status: "We're up and running!" });
-});
+    if (
+      req.header("internal-secret") !== process.env.SVT_INTERNAL_REQUEST_SECRET
+    ) {
+      return res
+        .status(401)
+        .json({ error: "Sorry, this is an invitation only kinda party." });
+    }
 
-const port = process.env.PORT || 1818;
+    try {
+      res.json({ status: "Processing request!" });
 
-app.listen(port, () =>
-  console.log(`Screenshot server listening on port ${port}`)
-);
+      const graphScreenshot = await cluster.execute(url);
+
+      // Add it to the blob store!
+      await put(btoa(url) + ".png", graphScreenshot, {
+        access: "public",
+        addRandomSuffix: false,
+      });
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({ error: "Unable to fetch image." });
+    }
+  });
+
+  app.head("/", async (req, res) => {
+    return res.json({ status: "We're up and running!" });
+  });
+
+  const port = process.env.PORT || 1818;
+
+  app.listen(port, () =>
+    console.log(`Screenshot server listening on port ${port}`)
+  );
+})();
