e77065b5ec3d510acedf9acf312d48dc0b2a3d43
Gabe Nunez <gabe@gabenunez.com>
2023-12-29 15:15:26 -0600

    Update vercel_url to now be returned

--- lib/utils.ts
+++ lib/utils.ts
@@ -24,7 +24,7 @@ export function getCurrentURL() {
   const { LOCAL_URL, VERCEL_URL } = process.env;
 
   if (LOCAL_URL) return LOCAL_URL;
-  return VERCEL_URL;
+  return "https://" + VERCEL_URL;
 }
 
 export async function internalFetchRequest<T>(
