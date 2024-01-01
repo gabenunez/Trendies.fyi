570fb42275e0ba2e67d07c9650f1182b94c85711
Gabe Nunez <gabe@gabenunez.com>
2024-01-01 14:35:38 -0600

    Add Vercel analytics for tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default function RootLayout({
         content="width=device-width, initial-scale=1, maximum-scale=1"
       />
       <body className={inter.className}>{children}</body>
+      <Analytics />
     </html>
   );
 }
